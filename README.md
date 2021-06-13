f056c2a4c796cf8e96e49004807b3c6c1d2ef482
lazyneo-setups64 <naufalramli06@gmail.com>
2021-06-13 20:42:56 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# hello-collection
+
+Just a collection of the classic "Hello world" program code (hehehe) in different programming languages that I wrote. So far I have wrote the same program in these languages:
+
+* C
+* Go
+* POSIX shell
+* Python
---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/* Basically a rickroll */
+int main (void) {
+	printf("Hello there, my friend!\n \nNEVER GONNA GIVE YOU UP,\nNEVER GONNA LET YOU DOWN,\nNEVER GONNA RUN AROUND AND DESERT YOU...\nNEVER GONNA MAKE YOU CRY,\nNEVER GONNA SAY GOODBYE,\nNEVER GONNA TELL A LIE AND HURT YOU... \n");
+	return 0;
+}
+
+/* EOF */
