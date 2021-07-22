f2d51e8b54e333ae558462113bcae55a4263431e
lazyneo-setups64 <naufalramli06@gmail.com>
2021-07-22 23:23:02 +0800

    Cleanup

--- hello.rs
+++ hello.rs
@@ -1,3 +1,4 @@
+/* Basically a rickroll */
 fn main() {
     println!("Hello there, my friend!\n \nNEVER GONNA GIVE YOU UP,\nNEVER GONNA LET YOU DOWN,\nNEVER GONNA RUN AROUND AND DESERT YOU...\nNEVER GONNA MAKE YOU CRY,\nNEVER GONNA SAY GOODBYE,\nNEVER GONNA TELL A LIE AND HURT YOU... \n");
 }
