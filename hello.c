1d9c70c7c5b1242b4627648a5f2e285bf21728f4
Neo64YT <naufalramli06@gmail.com>
2022-03-01 08:30:32 +0800

    Cleaned up hello.c

--- hello.c
+++ hello.c
@@ -3,8 +3,14 @@
 #include <stdio.h> // Include standard input/output header
 
 /* Basically a rickroll */ 
-int main () { // main() function
-	printf("Hello there, my friend!\n \nNEVER GONNA GIVE YOU UP,\nNEVER GONNA LET YOU DOWN,\nNEVER GONNA RUN AROUND AND DESERT YOU...\nNEVER GONNA MAKE YOU CRY,\nNEVER GONNA SAY GOODBYE,\nNEVER GONNA TELL A LIE AND HURT YOU... \n"); // This prints out the desired text on your terminal window
+int main () { 
+	printf("Hello there, my friend!\n");
+    printf("\nNEVER GONNA GIVE YOU UP,\n");
+    printf("NEVER GONNA LET YOU DOWN,\n");
+    printf("NEVER GONNA RUN AROUND AND DESERT YOU...\n");
+    printf("NEVER GONNA MAKE YOU CRY,\n");
+    printf("NEVER GONNA SAY GOODBYE,\n");
+    printf("NEVER GONNA TELL A LIE AND HURT YOU... \n"); 
 	return 0; // Say that the program is successfully executed
 }
 
